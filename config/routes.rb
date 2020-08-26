4fe67a231fa8656aaa8fe0730c8784df845074f8
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-26 12:26:13 +0900

    index-page made

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   
   root "quotes#index"
 
-  resources :quotes, only: :index
+  resources :quotes, only: [:index, :new, :create, :show]
+  resources :users, only: [:index] do
+    collection do
+      get 'myquote'
+    end
+  end
 end
