bcc8c83c834667b9129ed16d152851c9ef02106f
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-27 10:24:57 +0900

    follow function made

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :quotes
+
   has_many :favorites
   has_many :favorite_quotes, through: :favorites, source: :quote
 
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+
   validates :nickname, presence: true
+
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
 end
