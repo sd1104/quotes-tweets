0d24bf7ba44405aa60d9dd395458051e2887c93c
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-08-31 12:16:45 +0900

    seed files made

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/csv/users.csv', headers: true) do |row|
+  User.create(
+    nickname: row['nickname'],
+    email: row['email'],
+    password: '9L8UZjqWYpckAj8Yn+7QxuHDNZ/5nVih68yxf2JUwIuGdwPimalsFJCZV+c3T15YoFmr/cJcEf78Kz+4UJno6w'
+  )
+end
+
+CSV.foreach('db/csv/quotes.csv', headers: true) do |row|
+  Quote.create(
+    title: row['title'],
+    citation: row['citation'],
+    explanation: row['explanation'],
+    user_id: row['user_id']
+  )
+end
+
+CSV.foreach('db/csv/favorites.csv', headers: true) do |row|
+  Favorite.create(
+    user_id: row['user_id'],
+    quote_id: row['quote_id']
+  )
+end
+
+CSV.foreach('db/csv/relationships.csv', headers: true) do |row|
+  Relationship.create(
+    following_id: row['following_id'],
+    follower_id: row['follower_id']
+  )
+end
+
+CSV.foreach('db/csv/tags.csv', headers: true) do |row|
+  Tag.create(
+    id: row['id'],
+    name: row['name']
+  )
+end
+
+CSV.foreach('db/csv/quote_tags.csv', headers: true) do |row|
+  QuoteTag.create(
+    quote_id: row['quote_id'],
+    tag_id: row['tag_id']
+  )
+end
